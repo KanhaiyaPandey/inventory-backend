fd4c9b47de917fdf06aacdf43ed3c5588d81a482
KanhaiyaPandey <kanhaiyapandey2232@gmail.com>
2026-02-05 13:41:13 +0530

    fix: Code style issues

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import bcrypt from "bcrypt";
-import {prisma} from "../src/config/prisma"
+import { prisma } from "../src/config/prisma";
 
 async function main() {
   const password = await bcrypt.hash("supersecret123", 10);
