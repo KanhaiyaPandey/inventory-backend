c75b1313add598fd8171e0ad345af321bb853a06
nikhilmahato21 <nikhilmahato3337@gmail.com>
2026-02-05 13:19:49 +0530

    prisma bug fixes

--- src/modules/users/user.mapper.ts
+++ src/modules/users/user.mapper.ts
@@ -1,4 +1,4 @@
-import { User } from "../../../generated/prisma";
+import { User } from "@prisma/client";
 
 export const toPublicUser = (user: User) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
